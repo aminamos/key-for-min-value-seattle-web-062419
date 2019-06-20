9cf8795a0aa7e68bbd479f510aead36eedc9a613
Amin Amos <aminamosahs@gmail.com>
2019-06-20 17:52:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  a = notsorted[0]
+  b = notsorted[1]
+  c = notsorted[2]
   notsorted = name_hash.collect {|key, value| value}
   sortedstep2 = []
     if name_hash.length == 0
         nil
-    elsif(notsorted[0] > notsorted[1]) && (notsorted[1] > notsorted[2])
-      sortedstep2 = [notsorted[2], notsorted[1], notsorted[0]]
-    elsif (notsorted[0] < notsorted[1]) && (notsorted[1] < notsorted[2])
-      sortedstep2 = [notsorted[0], notsorted[1], notsorted[2]]
-    elsif  (notsorted[2] < notsorted[0]) && (notsorted[0] < notsorted[1])
-      sortedstep2 = [notsorted[2], notsorted[0], notsorted[1]]
+    elsif(a > b) && (b > c)
+      sortedstep2 = [c, b, a]
+    elsif (a < b) && (b < c)
+      sortedstep2 = [a, b, c]
+    elsif  (c < a) && (a < b)
+      sortedstep2 = [c, a, b]
     elsif ()
     elsif ()
     elsif ()
